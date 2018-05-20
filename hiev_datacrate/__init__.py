784baab9fd0f80ab91fd62e1ac734a1f583dae2b
Gerard Devine <g.devine@westernsydney.edu.au>
2018-05-20 23:59:11 +1000

    Added manifest file generator

--- hiev_datacrate/__init__.py
+++ hiev_datacrate/__init__.py
@@ -1,18 +1,45 @@
 import os
 import json
+import shutil
+from hiev_datacrate.utils import *
 from datetime import datetime
+
+
+BAGIT_VERSION = "0.97"
+TAGFILE_CHARACTER_ENCODING = "UTF-8"
 
 
 class DataCrate:
     """ A DataCrate Class """
-    def __init__(self):
+    def __init__(self, crate_name=None):
         self.catalog = None
+        self.crate_name = None
         self.crate_path = None
-        self.bagit_version = "0.97"
-        self.tagfile_char_encoding = "UTF-8"
+        self.datadir_path = None
+        self.bagit_version = BAGIT_VERSION
+        self.tagfile_char_encoding = TAGFILE_CHARACTER_ENCODING
+
+        # Create the basic structure of a DataCrate
+        if crate_name:
+            if not os.path.isdir(os.path.join(os.getcwd(), crate_name)):
+                self.crate_path = os.path.join(os.getcwd(), crate_name)
+                self.datadir_path = os.path.join(self.crate_path, 'data')
+                os.makedirs(self.crate_path)
+                os.makedirs(os.path.join(self.datadir_path))
+            else:
+                print('This datacrate already exists')
+        else:
+            crate_name = datetime.now().strftime('%Y%m%d%H%M%S')
+            self.crate_path = os.path.join(os.getcwd(), crate_name)
+            self.datadir_path = os.path.join(self.crate_path, 'data')
+            os.makedirs(self.crate_path)
+            os.makedirs(self.datadir_path)
 
     def set_catalog(self, catalog):
         self.catalog = catalog
+
+    def set_filegroup(self, filegroup):
+        self.filegroup = filegroup
 
     def validate(self):
         if self.catalog is None:
@@ -26,25 +53,23 @@ class DataCrate:
             file.write("BagIt-Version: " + self.bagit_version + "\n")
             file.write("Tag-File-Character-Encoding: " + self.tagfile_char_encoding + "\n")
 
-    def generate(self, crate_name=None):
-        if self.validate():
-            if crate_name:
-                if not os.path.isdir(os.path.join(os.getcwd(), crate_name)):
-                    self.crate_path = os.path.join(os.getcwd(), crate_name)
-                    os.makedirs(self.crate_path)
-                    os.makedirs(os.path.join(self.crate_path, 'data'))
-                else:
-                    print('This datacrate already exists')
-            else:
-                crate_name = datetime.now().strftime('%Y%m%d%H%M%S')
-                self.crate_path = os.path.join(os.getcwd(), crate_name)
-                os.makedirs(self.crate_path)
-                os.makedirs(os.path.join(self.crate_path, 'data'))
+    def ingest_files(self):
+        """ Copy files into the data folder"""
+        for file in self.filegroup.files:
+            shutil.copy(file.orig_path, os.path.join(self.crate_path, 'data'))
 
-            self.catalog.export(self.crate_path)
-            self.export_bt()
-        else:
-            return "Unable to generate an Invalid DataCrate"
+    def generate_manifest(self):
+        """ Generate manifest of md5 checksums """
+        file_path = os.path.join(self.crate_path, 'manifest-md5.txt')
+        with open(file_path, 'w') as manifest_file:
+            for file in os.listdir(self.datadir_path):
+                manifest_file.write(md5(os.path.join(self.datadir_path, file)) + " data/" + file + "\n")
+
+    def populate(self):
+        self.export_bt()
+        self.ingest_files()
+        self.catalog.export(self.crate_path)
+        self.generate_manifest()
 
 
 class Catalog:
@@ -103,3 +128,24 @@ class GraphElement:
             links.append({"@id": value})
 
         self.content[key] = links
+
+
+class FileGrouping:
+    """ A Grouping of Files """
+    def __init__(self, **kwargs):
+        self.description = kwargs['description']
+        self.files = []
+
+    def file_append(self, file_object):
+        """ Add a new file object """
+        self.files.append(file_object)
+
+
+class File:
+    """ An individual file object """
+    def __init__(self, orig_path, name, description, creator):
+        self.orig_path = orig_path
+        self.name = name
+        self.description = description
+        self.creator = creator
+
